efcdae9e8b82783e57921d31dc0b44fb124e6b8a
Intellmax <intell.bizz@gmail.com>
2017-07-12 09:20:11 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,9 @@ class Post (models.Model):
 
   def __str__(self):
   	return self.title
+  
+  def approved_comments(self):
+    return self.comments.filter(approved_comment=True)
   	
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
@@ -29,5 +32,3 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
 
-def approved_comments(self):
-    return self.comments.filter(approved_comment=True)
